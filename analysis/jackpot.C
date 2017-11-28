5a8dea37fdb169dda60951c6053067ae198493fc
Ran Bi <biran@mit.edu>
2017-11-29 00:41:03 +0100

    add preliminary macro to plot final results

---
+++ analysis/jackpot.C
@@ -0,0 +1,52 @@
+#include "TFile.h"
+#include "TH1.h"
+#include "TGraphErrors.h"
+#include "TColor.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+
+#include "include/errorband.h"
+
+int jackpot(const char* res, const char* sys, const char* label) {
+   TFile* fres = new TFile(res, "read");
+   TH1F* havg = (TH1F*)fres->Get("havg")->Clone();
+   TH1F* hsym = (TH1F*)fres->Get("hsym")->Clone();
+
+   TFile* fsys = new TFile(sys, "read");
+   TH1F* hsavg = (TH1F*)fsys->Get("havg_total")->Clone();
+   TH1F* hssym = (TH1F*)fsys->Get("hsym_total")->Clone();
+
+   TFile* fout = new TFile(Form("output/results-%s.root", label), "recreate");
+
+   TGraph* gr = new TGraph();
+
+   TCanvas* c1 = new TCanvas("c1", "", 600, 600);
+
+   havg->Draw("e x0");
+   draw_sys_unc(gr, havg, hsavg);
+   havg->Draw("e x0 same");
+
+   c1->SaveAs(Form("figs/results/results-avg-%s.png", label));
+
+   TCanvas* c2 = new TCanvas("c2", "", 600, 600);
+
+   hsym->Draw("e x0");
+   draw_sys_unc(gr, hsym, hssym);
+   hsym->Draw("e x0 same");
+
+   c2->SaveAs(Form("figs/results/results-sym-%s.png", label));
+
+   fout->Write("", TObject::kOverwrite);
+   fout->Close();
+
+   return 0;
+}
+
+int main(int argc, char* argv[]) {
+   if (argc == 4) {
+      return jackpot(argv[1], argv[2], argv[3]);
+   } else {
+      printf("usage: ./jackpot [results] [systematics] [label]\n");
+      return 1;
+   }
+}
