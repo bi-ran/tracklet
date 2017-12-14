b454d402e813cb1e5acfa1fc1a96283d49bf6d33
Ran Bi <biran@mit.edu>
2017-12-14 04:04:52 +0100

    rename j > z
    add placeholders for dndy results

--- analysis/zazen.C
+++ analysis/zazen.C
@@ -15,7 +15,7 @@ static const std::vector<int> colours = {
    COLOUR1, COLOUR3, COLOUR4, COLOUR5, COLOUR6, COLOUR2
 };
 
-int jackpot(const char* list, const char* hist, const char* label) {
+int zazen(const char* list, const char* hist, const char* label) {
    std::vector<std::string> rlist;
    std::ifstream fstream(list);
    if (fstream) {
@@ -53,11 +53,14 @@ int jackpot(const char* list, const char* hist, const char* label) {
 
    TH1F* hframe = (TH1F*)fres[0]->Get("hframe")->Clone();
 
-   TH1F* hres[nres];
-   TH1F* hsys[nres];
+   TH1F* hres[nres]; TH1F* hsys[nres];
+   TH1F* hyres[nres]; TH1F* hysys[nres];
    for (std::size_t i = 0; i < nres; ++i) {
       hres[i] = (TH1F*)fres[i]->Get(hist)->Clone(Form("%s_%s", hist, labels[i].c_str()));
       hsys[i] = (TH1F*)fsys[i]->Get(Form("%s_tdiff", hist))->Clone(Form("%s_%s_tdiff", hist, labels[i].c_str()));
+
+      hyres[i] = (TH1F*)hres[i]->Clone(Form("%sy_%s", hist, labels[i].c_str()));
+      hysys[i] = (TH1F*)hsys[i]->Clone(Form("%sy_%s_tdiff", hist, labels[i].c_str()));
    }
 
    TGraph* gr = new TGraph();
@@ -94,6 +97,37 @@ int jackpot(const char* list, const char* hist, const char* label) {
 
    c1->SaveAs(Form("figs/results/results-%s-%s.png", hist, label));
 
+   TCanvas* c2 = new TCanvas("c2", "", 400, 400);
+   gPad->SetLogy();
+
+   hframe->SetTitle(";y;dN/dy");
+   hframe->Draw();
+
+   for (std::size_t i = 0; i < nres; ++i) {
+      hyres[i]->SetMarkerStyle(20);
+      hyres[i]->SetMarkerSize(0.64);
+      hyres[i]->SetMarkerColor(colours[i % colours.size()]);
+      hyres[i]->SetLineColor(colours[i % colours.size()]);
+
+      gr->SetFillColorAlpha(colours[i % colours.size()], 0.4);
+      draw_sys_unc(gr, hyres[i], hysys[i]);
+      hyres[i]->Draw("e x0 same");
+   }
+
+   watermark();
+
+   TLegend* l2 = new TLegend(0.4, 0.3, 0.6, 0.475);
+   l2->SetTextFont(43);
+   l2->SetTextSize(12);
+   l2->SetBorderSize(0);
+   l2->SetFillStyle(0);
+   l2->AddEntry((TObject*)0, "XeXe", "");
+   for (std::size_t i = 0; i < nres; ++i)
+      l2->AddEntry(hyres[i], legends[i].c_str(), "p");
+   l2->Draw();
+
+   c2->SaveAs(Form("figs/results/results-%s-%s-y.png", hist, label));
+
    fout->Write("", TObject::kOverwrite);
    fout->Close();
 
@@ -102,9 +136,9 @@ int jackpot(const char* list, const char* hist, const char* label) {
 
 int main(int argc, char* argv[]) {
    if (argc == 4) {
-      return jackpot(argv[1], argv[2], argv[3]);
+      return zazen(argv[1], argv[2], argv[3]);
    } else {
-      printf("usage: ./jackpot [list] [hist] [label]\n");
+      printf("usage: ./zazen [list] [hist] [label]\n");
       return 1;
    }
 }
