dd7df230aa07e3b2c70963c790151a0e76ab45b9
Ran Bi <biran@mit.edu>
2017-12-14 22:32:58 +0100

    standardise info struct and function prototypes
    cleaner code

--- analysis/harvest_hists.C
+++ analysis/harvest_hists.C
@@ -56,36 +56,35 @@ static const std::vector<int> colour = {
 };
 static const int ncolours = colour.size();
 
-typedef struct oned_t {
-   std::string id;
-   std::string label;
-   std::string var;
-   float       bins[3];
-   int         csize[2];
-   bool        custom;
-   std::string sel;
-   std::string gopt;
-} oned_t;
+typedef struct varinfo_t {
+   std::string                         id;
+   std::vector<std::string>            label;
+   std::vector<std::string>            var;
+   std::vector<std::array<float, 3>>   bins;
+   int                                 csize[2];
+   int                                 custom;
+   std::string                         sel;
+   std::string                         gopt;
+} varinfo_t;
 
-static const std::vector<oned_t> options_pixel_oned = {
+static const std::vector<varinfo_t> options_pixel_1d = {
    {
-      "cs", "cluster size", "cs",
-      {50, 0, 50},
+      "cs", {"cluster size"}, {"cs"},
+      {{50, 0, 50}},
       {600, 600}, 1, "(1)", ""
    }, {
-      "ch", "cluster charge", "ch",
-      {100, 0, 10000000},
+      "ch", {"cluster charge"}, {"ch"},
+      {{100, 0, 10000000}},
       {600, 600}, 1, "(1)", ""
    }, {
-      "nhits", "number of pixel hits", "nhits",
-      {100, 0, 15000},
+      "nhits", {"number of pixel hits"}, {"nhits"},
+      {{100, 0, 15000}},
       {600, 600}, 1, "(1)", ""
    }
 };
 
-int compare_pixels(const char* list, const char* label, int opt) {
-   std::vector<oned_t> options = options_pixel_oned;
-
+int compare_pixels(std::vector<varinfo_t> const& options,
+      const char* list, const char* label, int opt) {
    std::vector<std::string> flist;
    std::ifstream fstream(list);
    if (fstream) {
@@ -111,7 +110,7 @@ int compare_pixels(const char* list, const char* label, int opt) {
    TCut fsel = OS(sel);
 
    const char* idstr = OS(id);
-   const char* varstr = OS(var);
+   const char* varstr = OS(var[0]);
 
    TH1::SetDefaultSumw2();
    gStyle->SetOptStat(0);
@@ -126,8 +125,8 @@ int compare_pixels(const char* list, const char* label, int opt) {
    TH1D* h##q[nfiles];                                                        \
    for (std::size_t j = 0; j < nfiles; ++j) {                                 \
       h##q[j] = new TH1D(Form("hp" #q "f%zu%s", j, idstr),                    \
-            Form(";%s (layer " #q ");", OS(label)),                           \
-            (int)OPT(bins[0]), OPT(bins[1]), OPT(bins[2]));                   \
+            Form(";%s (layer " #q ");", OS(label[0])),                        \
+            (int)OPT(bins[0][0]), OPT(bins[0][1]), OPT(bins[0][2]));          \
    }                                                                          \
 
    PIXELS(SETUP_1D_PIXELS)
@@ -179,37 +178,36 @@ int compare_pixels(const char* list, const char* label, int opt) {
    return 0;
 }
 
-static const std::vector<oned_t> options_tracklet_oned = {
+static const std::vector<varinfo_t> options_tracklet_1d = {
    {
-      "deta", "#Delta#eta", "deta",
-      {100, -0.5, 0.5},
+      "deta", {"#Delta#eta"}, {"deta"},
+      {{100, -0.5, 0.5}},
       {600, 600}, 1, "abs(deta)<0.5", ""
    }, {
-      "dphi", "#Delta#phi", "dphi",
-      {100, 0, 0.5},
+      "dphi", {"#Delta#phi"}, {"dphi"},
+      {{100, 0, 0.5}},
       {600, 600}, 1, "abs(dphi)<0.5", ""
    }, {
-      "dr2", "#Deltar^{2}", "dr2",
-      {100, 0, 0.25},
+      "dr2", {"#Deltar^{2}"}, {"dr2"},
+      {{100, 0, 0.25}},
       {600, 600}, 1, "abs(dr2)<0.25", ""
    }, {
-      "dr", "#Deltar", "sqrt(dr2)",
-      {100, 0, 0.5},
+      "dr", {"#Deltar"}, {"sqrt(dr2)"},
+      {{100, 0, 0.5}},
       {600, 600}, 1, "abs(dr2)<0.25", ""
    }, {
-      "vz", "v_{z}", "vz[1]",
-      {100, -15, 15},
+      "vz", {"v_{z}"}, {"vz[1]"},
+      {{100, -15, 15}},
       {600, 600}, 0, "(1)", ""
    }, {
-      "ntracklet", "number of tracklets", "ntracklet",
-      {100, 0, 10000},
+      "ntracklet", {"number of tracklets"}, {"ntracklet"},
+      {{100, 0, 10000}},
       {600, 600}, 1, "(1)", ""
    }
 };
 
-int compare_tracklets(const char* list, const char* label, int opt) {
-   std::vector<oned_t> options = options_tracklet_oned;
-
+int compare_tracklets(std::vector<varinfo_t> const& options,
+      const char* list, const char* label, int opt) {
    std::vector<std::string> flist;
    std::ifstream fstream(list);
    if (fstream) {
@@ -237,7 +235,7 @@ int compare_tracklets(const char* list, const char* label, int opt) {
    fsel *= "weight";
 
    const char* idstr = OS(id);
-   const char* varstr = OS(var);
+   const char* varstr = OS(var[0]);
 
    TH1::SetDefaultSumw2();
    gStyle->SetOptStat(0);
@@ -251,8 +249,8 @@ int compare_tracklets(const char* list, const char* label, int opt) {
    for (std::size_t j = 0; j < nfiles; ++j) {                                 \
       t##q##w[j] = (TTree*)f[j]->Get("TrackletTree" #q #w);                   \
       h##q##w[j] = new TH1D(Form("ht" #q #w "f%zu%s", j, idstr),              \
-            Form(";%s (layers " #q "+" #w ");", OS(label)),                   \
-            (int)OPT(bins[0]), OPT(bins[1]), OPT(bins[2]));                   \
+            Form(";%s (layers " #q "+" #w ");", OS(label[0])),                \
+            (int)OPT(bins[0][0]), OPT(bins[0][1]), OPT(bins[0][2]));          \
    }                                                                          \
 
    TRACKLETS(SETUP_1D_TRACKLETS)
@@ -304,61 +302,47 @@ int compare_tracklets(const char* list, const char* label, int opt) {
    return 0;
 }
 
-typedef struct twod_t {
-   std::string id;
-   std::string label[2];
-   std::string xvar;
-   float       xbins[3];
-   std::string yvar;
-   float       ybins[3];
-   int         csize[2];
-   int         custom;
-   std::string sel;
-   std::string gopt;
-} twod_t;
-
-static const std::vector<twod_t> options_pixel_twod = {
+static const std::vector<varinfo_t> options_pixel_2d = {
    {
       "eta-phi", {"#eta", "#phi"},
-      "eta@", {1000, -4, 4},
-      "phi@", {1000, -4, 4},
+      {"eta@", "phi@"},
+      {{1000, -4, 4}, {1000, -4, 4}},
       {600, 600}, 0x23, "(1)", "colz"
    }, {
       "eta-r", {"#eta", "r"},
-      "eta@", {1000, -4, 4},
-      "r@", {1000, 0, 20},
+      {"eta@", "r@"},
+      {{1000, -4, 4}, {1000, 0, 20}},
       {600, 600}, 0x33, "(1)", "colz"
    }, {
       "eta-cs", {"#eta", "cluster size"},
-      "eta@", {200, -4, 4},
-      "cs@", {40, 0, 40},
+      {"eta@", "cs@"},
+      {{200, -4, 4}, {40, 0, 40}},
       {600, 600}, 0x03, "(1)", "colz"
    }, {
       "x-y", {"x", "y"},
-      "r@*cos(phi@)", {1000, -20, 20},
-      "r@*sin(phi@)", {1000, -20, 20},
+      {"r@*cos(phi@)", "r@*sin(phi@)"},
+      {{1000, -20, 20}, {1000, -20, 20}},
       {600, 600}, 0x13, "(1)", "colz"
    }, {
       "z-phi", {"z", "#phi"},
-      "r@/tan(2*atan(exp(-eta@)))", {1000, -30, 30},
-      "phi@", {1000, -4, 4},
+      {"r@/tan(2*atan(exp(-eta@)))", "phi@"},
+      {{1000, -30, 30}, {1000, -4, 4}},
       {600, 2400}, 0x01, "(1)", "colz"
    }, {
       "z-r", {"z", "r"},
-      "r@/tan(2*atan(exp(-eta@)))", {1000, -30, 30},
-      "r@", {1000, 0, 20},
+      {"r@/tan(2*atan(exp(-eta@)))", "r@"},
+      {{1000, -30, 30}, {1000, 0, 20}},
       {600, 600}, 0x33, "(1)", "colz"
    }, {
       "fpix-z-phi", {"z", "#phi"},
-      "r@/tan(2*atan(exp(-eta@)))", {1000, -60, 60},
-      "phi@", {1000, -4, 4},
+      {"r@/tan(2*atan(exp(-eta@)))", "phi@"},
+      {{1000, -60, 60}, {1000, -4, 4}},
       {1200, 600}, 0x22, "(1)", "colz"
    }
 };
 
-int map_pixels(const char* input, const char* label, int opt) {
-   std::vector<twod_t> options = options_pixel_twod;
-
+int map_pixels(std::vector<varinfo_t> const& options,
+      const char* input, const char* label, int opt) {
    TCut fsel = OS(sel);
 
    const char* l0str = OS(label[0]);
@@ -371,20 +355,20 @@ int map_pixels(const char* input, const char* label, int opt) {
    TTree* t = (TTree*)f->Get("pixel/PixelTree");
 
 #define SETUP_2D_PIXELS(q)                                                    \
-   std::string xvar##q = OPT(xvar);                                           \
-   std::string yvar##q = OPT(yvar);                                           \
-   std::replace(xvar##q.begin(), xvar##q.end(), '@', #q[0]);                  \
-   std::replace(yvar##q.begin(), yvar##q.end(), '@', #q[0]);                  \
+   std::string x##q = OPT(var[0]);                                            \
+   std::string y##q = OPT(var[1]);                                            \
+   std::replace(x##q.begin(), x##q.end(), '@', #q[0]);                        \
+   std::replace(y##q.begin(), y##q.end(), '@', #q[0]);                        \
                                                                               \
    TH2D* h##q = new TH2D(Form("h" #q "%s", idstr),                            \
          Form(";%s (layer " #q ");%s (layer " #q ")", l0str, l1str),          \
-         (int)OPT(xbins[0]), OPT(xbins[1]), OPT(xbins[2]),                    \
-         (int)OPT(ybins[0]), OPT(ybins[1]), OPT(ybins[2]));                   \
+         (int)OPT(bins[0][0]), OPT(bins[0][1]), OPT(bins[0][2]),              \
+         (int)OPT(bins[1][0]), OPT(bins[1][1]), OPT(bins[1][2]));             \
 
    PIXELS(SETUP_2D_PIXELS)
 
 #define DRAW_2D_PIXELS(q)                                                     \
-   t->Draw(Form("%s:%s>>h" #q "%s", CS(yvar##q), CS(xvar##q), idstr),         \
+   t->Draw(Form("%s:%s>>h" #q "%s", CS(y##q), CS(x##q), idstr),               \
          fsel, "goff");                                                       \
                                                                               \
    TCanvas* c##q = new TCanvas("c" #q, "", OPT(csize[0]), OPT(csize[1]));     \
@@ -397,8 +381,8 @@ int map_pixels(const char* input, const char* label, int opt) {
    if (OPT(custom) & 0x2) { FPIX(DRAW_2D_PIXELS) }
 
    TH2D* hall = new TH2D(Form("hall%s", idstr), Form(";%s;%s", l0str, l1str),
-         (int)OPT(xbins[0]), OPT(xbins[1]), OPT(xbins[2]),
-         (int)OPT(ybins[0]), OPT(ybins[1]), OPT(ybins[2]));
+         (int)OPT(bins[0][0]), OPT(bins[0][1]), OPT(bins[0][2]),
+         (int)OPT(bins[1][0]), OPT(bins[1][1]), OPT(bins[1][2]));
 
    if (OPT(custom) >> 4) {
 
@@ -429,23 +413,22 @@ int map_pixels(const char* input, const char* label, int opt) {
    return 0;
 }
 
-static const std::vector<twod_t> options_tracklet_twod = {
+static const std::vector<varinfo_t> options_tracklet_2d = {
    {
       "eta-phi", {"#eta", "#phi"},
-      "eta1", {1000, -4, 4},
-      "phi1", {1000, -4, 4},
+      {"eta1", "phi1"},
+      {{1000, -4, 4}, {1000, -4, 4}},
       {600, 600}, 0, "(1)", "colz"
    }, {
       "eta-vz", {"#eta", "v_{z}"},
-      "eta1", {200, -4, 4},
-      "vz[1]", {200, -20, 20},
+      {"eta1", "vz[1]"},
+      {{200, -4, 4}, {200, -20, 20}},
       {600, 600}, 0, "(1)", "colz"
    }
 };
 
-int map_tracklets(const char* input, const char* label, int opt) {
-   std::vector<twod_t> options = options_tracklet_twod;
-
+int map_tracklets(std::vector<varinfo_t> const& options,
+      const char* input, const char* label, int opt) {
    TCut fsel = OS(sel);
    fsel = fsel && "abs(vz[1])<15 && hlt";
    fsel *= "weight";
@@ -463,13 +446,13 @@ int map_tracklets(const char* input, const char* label, int opt) {
    TH2D* h##q##w = new TH2D(Form("h" #q #w "%s", idstr),                      \
          Form(";%s (layer " #q "+" #w ");%s (layer " #q "+" #w ")",           \
                l0str, l1str),                                                 \
-         OPT(xbins[0]), OPT(xbins[1]), OPT(xbins[2]),                         \
-         OPT(ybins[0]), OPT(ybins[1]), OPT(ybins[2]));                        \
+         OPT(bins[0][0]), OPT(bins[0][1]), OPT(bins[0][2]),                   \
+         OPT(bins[1][0]), OPT(bins[1][1]), OPT(bins[1][2]));                  \
 
    TRACKLETS(SETUP_2D_TRACKLETS)
 
 #define DRAW_2D_TRACKLETS(q, w)                                               \
-   t##q##w->Draw(Form("%s:%s>>h" #q #w "%s", OS(yvar), OS(xvar), idstr),      \
+   t##q##w->Draw(Form("%s:%s>>h" #q #w "%s", OS(var[1]), OS(var[0]), idstr),  \
          fsel, "goff");                                                       \
                                                                               \
    TCanvas* c##q##w = new TCanvas("c" #q #w, "", 600, 600);                   \
@@ -493,65 +476,36 @@ int map_tracklets(const char* input, const char* label, int opt) {
 }
 
 int main(int argc, char* argv[]) {
+   std::vector<std::vector<varinfo_t> const*> options = {
+      &options_pixel_1d, &options_tracklet_1d,
+      &options_pixel_2d, &options_tracklet_2d
+   };
+
+   std::vector<int (*)(std::vector<varinfo_t> const&,
+         const char*, const char*, int)> delegates = {
+      compare_pixels, compare_tracklets,
+      map_pixels, map_tracklets
+   };
+
+   std::vector<std::string> usagestrs = {
+      "list", "list",
+      "pixel", "tracklet"
+   };
+
    if (argc > 1) {
       int mode = atoi(argv[1]);
-      
-      switch (mode) {
-         case 0:
-            if (argc == 4) {
-               for (std::size_t i = 0; i < options_pixel_oned.size(); ++i)
-                  compare_pixels(argv[2], argv[3], i);
-            } else if (argc > 4) {
-               for (int i = 4; i < argc; ++i) {
-                  compare_pixels(argv[2], argv[3], atoi(argv[i]));
-               }
-            } else {
-               printf("usage: ./harvest_hists [0] [list] [label] (opt)\n");
-               return 1;
-            }
-            break;
-         case 1:
-            if (argc == 4) {
-               for (std::size_t i = 0; i < options_tracklet_oned.size(); ++i)
-                  compare_tracklets(argv[2], argv[3], i);
-            } else if (argc > 4) {
-               for (int i = 4; i < argc; ++i) {
-                  compare_tracklets(argv[2], argv[3], atoi(argv[i]));
-               }
-            } else {
-               printf("usage: ./harvest_hists [1] [list] [label] (opt)\n");
-               return 1;
-            }
-            break;
-         case 2:
-            if (argc == 4) {
-               for (std::size_t i = 0; i < options_pixel_twod.size(); ++i)
-                  map_pixels(argv[2], argv[3], i);
-            } else if (argc > 4) {
-               for (int i = 4; i < argc; ++i) {
-                  map_pixels(argv[2], argv[3], atoi(argv[i]));
-               }
-            } else {
-               printf("usage: ./harvest_hists [2] [pixel] [label] (opt)\n");
-               return 1;
-            }
-            break;
-         case 3:
-            if (argc == 4) {
-               for (std::size_t i = 0; i < options_tracklet_twod.size(); ++i)
-                  map_tracklets(argv[2], argv[3], i);
-            } else if (argc > 4) {
-               for (int i = 4; i < argc; ++i) {
-                  map_tracklets(argv[2], argv[3], atoi(argv[i]));
-               }
-            } else {
-               printf("usage: ./harvest_hists [3] [tracklet] [label] (opt)\n");
-               return 1;
-            }
-            break;
-      }
 
-      return 0;
+      if (argc == 4) {
+         for (std::size_t i = 0; i < options[mode]->size(); ++i)
+            (*delegates[mode])(*options[mode], argv[2], argv[3], i);
+      } else if (argc > 4) {
+         for (int i = 4; i < argc; ++i)
+            (*delegates[mode])(*options[mode], argv[2], argv[3], atoi(argv[i]));
+      } else {
+         printf("usage: ./harvest_hists [%i] [%s] [label] (opt)\n",
+               mode, usagestrs[mode].c_str());
+         return 1;
+      }
    } else {
       printf("usage: ./harvest_hists [mode]\n");
       return 1;
