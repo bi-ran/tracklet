f973af861a3c9f92483de50cd27d0d913db09c8b
Ran Bi <biran@mit.edu>
2018-01-03 02:29:19 +0100

    cmssw: reset npart for each event

--- cmssw/Arboretum/PixelPlant/plugins/PixelPlant.cc
+++ cmssw/Arboretum/PixelPlant/plugins/PixelPlant.cc
@@ -401,6 +401,7 @@ void PixelPlant::fill_particles(const edm::Event& iEvent) {
 
    pix_.process = generator->signal_process_id();
 
+   pix_.npart = 0;
    for (auto it = generator->particles_begin(); it != generator->particles_end(); ++it) {
       if ((*it)->status() != 1) { continue; }
       if (fabs((*it)->momentum().eta()) > 3) { continue; }
