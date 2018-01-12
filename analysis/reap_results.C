981a7ab4a0642b80a00518e39bd54dbb1976bf73
Ran Bi <biran@mit.edu>
2018-01-12 19:46:06 +0100

    adjust alpha fit parameters
    coerce shape

--- analysis/reap_results.C
+++ analysis/reap_results.C
@@ -116,7 +116,7 @@ int reap_results(int type,
    for (int i=0; i<neta; i++) {
       for (int j=0; j<nvz; j++) {
          h1alpha[i][j] = new TH1F(Form("alpha_%i_%i", i, j), "", nmult, multb);
-         falpha[i][j] = new TF1(Form("falpha_%i_%i", i, j), "[0]+[1]/(x+[2])+[3]/(x*x)+[4]*x", 25, 10000);
+         falpha[i][j] = new TF1(Form("falpha_%i_%i", i, j), "[0]+[1]/(x+[2])+[3]/(x*x)+exp([4])*x", 25, 10000);
       }
    }
 
@@ -265,10 +265,10 @@ int reap_results(int type,
       for (int j=0; j<nvz; j++) {
          for (int i=0; i<neta; i++) {
             falpha[i][j]->SetParameter(0, 0.84);
-            falpha[i][j]->SetParLimits(1, 0, 9999);
-            falpha[i][j]->SetParLimits(2, 0, 9999);
-            falpha[i][j]->SetParLimits(3, -9999, 0);
-            falpha[i][j]->SetParLimits(4, 0, 0.0001);
+            falpha[i][j]->SetParLimits(1, 1, 256);
+            falpha[i][j]->SetParLimits(2, 0, 512);
+            falpha[i][j]->SetParLimits(3, -216, 0);
+            falpha[i][j]->SetParLimits(4, -128, -8);
 
             h1alpha[i][j]->Fit(falpha[i][j], "M Q", "", 25, 10000);
             h1alpha[i][j]->Fit(falpha[i][j], "M E Q", "", 25, 10000);
