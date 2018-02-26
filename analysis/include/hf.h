ed4391d2de716b47c70e353be91a2971ea2bd1a7
Ran Bi <biran@mit.edu>
2018-02-26 23:23:36 +0100

    add other centrality tables

--- analysis/include/hf.h
+++ analysis/include/hf.h
@@ -10,6 +10,24 @@ static const float hfofficial[21] = {
     10000.00
 };
 
+/* CentralityTable_HFtowers200_DataXeXe_eff100_run2v941x01_offline */
+// static const float hfofficial[21] = {
+//         0.00,  18.715, 26.8853, 38.8775,  55.839,
+//       79.171, 110.752, 153.039, 208.026, 277.951,
+//      364.262, 470.097, 596.901, 747.509, 924.333,
+//      1132.31, 1375.09, 1661.39, 2003.11, 2419.56,
+//     10000.00
+// };
+
+/* CentralityTable_HFtowers200_XeXe5p44TeVEPOSLHC_v941x01_mc */
+// static const float hfofficial[21] = {
+//         0.00, 12.4995, 17.3889, 25.2556, 37.7678,
+//      54.8032, 77.9654, 109.395, 152.187, 205.717,
+//        272.2, 356.406, 459.404, 582.857, 731.719,
+//      906.561, 1128.65, 1406.31,  1733.3, 2184.99,
+//     10000.00
+// };
+
 /* CentralityTable_HFtowers200_DataXeXe_eff942_run2v9313x02_offline */
 // static const float hfofficial[21] = {
 //         0.00,    0.00,  16.104, 22.5195, 33.3297,
