debbb859caf3430e610972cf6af4537d15167338
Ran Bi <biran@mit.edu>
2018-03-17 23:35:29 +0100

    ss: misc changes
    reduce scope of variables
    add labels to figures

--- analysis/sum_systematics.C
+++ analysis/sum_systematics.C
@@ -20,6 +20,7 @@ int sum_systematics(const char* config, const char* label) {
 
     auto files = conf->get<std::vector<std::string>>("files");
     auto tags = conf->get<std::vector<std::string>>("tags");
+    auto labels = conf->get<std::vector<std::string>>("labels");
     auto dffs = conf->get<std::vector<std::string>>("diff-fit-funcs");
     auto rffs = conf->get<std::vector<std::string>>("ratio-fit-funcs");
     auto options = conf->get<std::vector<int>>("options");
@@ -29,50 +30,55 @@ int sum_systematics(const char* config, const char* label) {
     if (!nfiles) { printf("error: no files provided!\n"); return 1; }
     if (!nhists) { printf("error: no histograms listed!\n"); return 1; }
 
-    TFile* f0 = new TFile(nominal.c_str(), "read");
+    TFile* f0 = new TFile(nominal.data(), "read");
     TFile* f[nfiles];
     for (std::size_t i = 0; i < nfiles; ++i)
-        f[i] = new TFile(files[i].c_str(), "read");
+        f[i] = new TFile(files[i].data(), "read");
 
     TFile* fout = new TFile(Form("output/systematics-%s.root", label), "recreate");
 
     TH1::SetDefaultSumw2();
     gStyle->SetOptStat(0);
 
-    varsum* tvars[nhists];
-    varone* svars[nhists][nfiles];
-
-    TH1F* h0[nhists]; TH1F* h[nhists][nfiles];
     for (std::size_t i = 0; i < nhists; ++i) {
-        h0[i] = (TH1F*)f0->Get(histograms[i].c_str())->Clone(
-                Form("%s0", histograms[i].c_str()));
+        TH1F* h0 = (TH1F*)f0->Get(histograms[i].data())->Clone(
+                Form("%s0", histograms[i].data()));
 
-        tvars[i] = new varsum(histograms[i].c_str(), h0[i]);
-        std::vector<varsum*> vargroups;
+        std::vector<varone*> svars;
+        std::map<int, varsum*> vargroups;
 
         for (std::size_t j = 0; j < nfiles; ++j) {
-            h[i][j] = (TH1F*)f[j]->Get(histograms[i].c_str())->Clone(
-                    Form("%s%s", histograms[i].c_str(), tags[j].c_str()));
+            TH1F* h1 = (TH1F*)f[j]->Get(histograms[i].data())->Clone(
+                    Form("%s%s", histograms[i].data(), tags[j].data()));
 
-            svars[i][j] = new varone(histograms[i], tags[j], h0[i], h[i][j]);
-            svars[i][j]->fit(dffs[j].c_str(), rffs[j].c_str(), 0.1);
-            svars[i][j]->write();
+            svars.push_back(new varone(histograms[i], tags[j], h0, h1));
+            svars.back()->fit(dffs[j].data(), rffs[j].data(), 0.1);
+            svars.back()->write();
 
-            if (groups[j] > vargroups.size())
-                vargroups.push_back(new varsum(histograms[i].c_str(), h0[i]));
+            uint32_t group = groups[j];
+            if (vargroups.find(group) == std::end(vargroups))
+                vargroups.emplace(group, new varsum(histograms[i].data(), h0));
 
-            if (!groups[j]) { tvars[i]->add(svars[i][j], options[j]); }
-            else { vargroups[groups[j] - 1]->max(svars[i][j], options[j]); }
+            switch (group) {
+                case 0:
+                    vargroups[group]->add(svars.back(), options[j]);
+                    break;
+                default:
+                    vargroups[group]->max(svars.back(), options[j]);
+                    break;
+            }
         }
 
+        varsum *tvars = new varsum(histograms[i].data(), h0);
         for (const auto& vargroup : vargroups)
-            tvars[i]->add(vargroup);
+            tvars->add(vargroup.second);
 
-        tvars[i]->write();
+        tvars->write();
 
-        TH1F* h;
+        TH1F* h; TLatex* l = new TLatex();
+        l->SetTextFont(43); l->SetTextSize(13);
         std::string path = Form("figs/uncertainties/systematics-%s-%s",
-                label, histograms[i].c_str());
+                label, histograms[i].data());
 
         int nrows = nfiles / 3 + 1;
         TCanvas* c0 = new TCanvas("scratch", "", 600, 600);
@@ -81,31 +87,37 @@ int sum_systematics(const char* config, const char* label) {
         c1->Divide(3, nrows); c2->Divide(3, nrows);
 
         for (std::size_t j = 0; j < nfiles; ++j) {
-            h = svars[i][j]->adiff(0); htitle(h, ";#eta;difference");
-            c0->cd(); h->Draw(); c1->cd(j+1); h->Draw();
-            c0->SaveAs(Form("%s-%s-diff.png", path.c_str(), tags[j].c_str()));
+            h = svars[j]->adiff(0); htitle(h, ";#eta;difference");
+            c0->cd(); h->Draw(); l->DrawLatexNDC(0.4, 0.8, labels[j].data());
+            c1->cd(j+1); h->Draw(); l->DrawLatexNDC(0.4, 0.8, labels[j].data());
+            c0->SaveAs(Form("%s-%s-diff.png", path.data(), tags[j].data()));
 
-            h = svars[i][j]->aratio(0); htitle(h, ";#eta;ratio");
-            c0->cd(); h->Draw(); c2->cd(j+1); h->Draw();
-            c0->SaveAs(Form("%s-%s-ratio.png", path.c_str(), tags[j].c_str()));
+            h = svars[j]->aratio(0); htitle(h, ";#eta;ratio");
+            c0->cd(); h->Draw(); l->DrawLatexNDC(0.4, 0.8, labels[j].data());
+            c2->cd(j+1); h->Draw(); l->DrawLatexNDC(0.4, 0.8, labels[j].data());
+            c0->SaveAs(Form("%s-%s-ratio.png", path.data(), tags[j].data()));
         }
 
-        h = tvars[i]->diff(); htitle(h, ";#eta;difference");
-        c0->cd(); h->Draw("p hist e"); c1->cd(nfiles+1); h->Draw("p hist e");
-        c0->SaveAs(Form("%s.total-diff.png", path.c_str()));
-        c1->SaveAs(Form("%s.all-diff.png", path.c_str()));
+        h = tvars->diff(); htitle(h, ";#eta;difference");
+        c0->cd(); h->Draw("p hist e");
+        l->DrawLatexNDC(0.4, 0.8, "total uncertainty");
+        c1->cd(nfiles+1); h->Draw("p hist e");
+        l->DrawLatexNDC(0.4, 0.8, "total uncertainty");
+        c0->SaveAs(Form("%s.total-diff.png", path.data()));
+        c1->SaveAs(Form("%s.all-diff.png", path.data()));
 
-        h = tvars[i]->ratio(); htitle(h, ";#eta;ratio");
-        c0->cd(); h->Draw("p hist e"); c2->cd(nfiles+1); h->Draw("p hist e");
-        c0->SaveAs(Form("%s.total-ratio.png", path.c_str()));
-        c2->SaveAs(Form("%s.all-ratio.png", path.c_str()));
+        h = tvars->ratio(); htitle(h, ";#eta;ratio");
+        c0->cd(); h->Draw("p hist e");
+        l->DrawLatexNDC(0.4, 0.8, "total");
+        c2->cd(nfiles+1); h->Draw("p hist e");
+        l->DrawLatexNDC(0.4, 0.8, "total");
+        c0->SaveAs(Form("%s.total-ratio.png", path.data()));
+        c2->SaveAs(Form("%s.all-ratio.png", path.data()));
 
         delete c0; delete c1; delete c2;
-        for (std::size_t j = 0; j < nfiles; ++j)
-            delete svars[i][j];
-        for (const auto& vargroup : vargroups)
-            delete vargroup;
-        delete tvars[i];
+        for (const auto& svar : svars) { delete svar; }
+        for (const auto& vargroup : vargroups) { delete vargroup.second; }
+        delete tvars;
     }
 
     fout->Write("", TObject::kOverwrite);
